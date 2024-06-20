571c648c89e16877e5d1b6810408bf50db686c4e
bek123456789 <redmirating@gmail.com>
2024-06-20 16:18:54 +0500

    done

---
+++ main.js
@@ -0,0 +1,127 @@
+// 1 FAYL  1-CHALENGE
+
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+
+
+
+
+// const BMImark = massMark / massMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHighnerBMI = BMImark > BMIJohn;
+
+
+// console.log(BMImark, BMIJohn, markHighnerBMI);
+
+
+
+// 2-CHALENGE
+
+
+// const scoreDelphins = (21 + 32 + 55 + 102 + 70) / 3;
+// const scoreKoalas = (2 + 37 + 58 + 153 + 70) / 3;
+
+// console.log(scoreDelphins, scoreKoalas);
+
+// if (scoreDelphins > scoreKoalas && scoreDelphins >= 100) {
+//     console.log('Delphin is winner!')
+// }
+// else if (scoreKoalas > scoreDelphins && scoreKoalas >= 100) {
+//     console.log('Koalas is winner!');
+// }
+// else if (scoreDelphins === scoreKoalas) {
+//     console.log('No winner');
+// }
+
+// 3-CHALENGE
+// const scoreDelphins = (21 + 32 + 55 + 102 + 70) / 3;
+// const scoreKoalas = (2 + 37 + 58 + 153 + 70) / 3;
+
+// console.log(scoreDelphins, scoreKoalas);
+
+// if (scoreDelphins > scoreKoalas && scoreDelphins >= 100) {
+//     console.log('Delphin is winner!')
+// }
+// else if (scoreKoalas > scoreDelphins && scoreKoalas >= 100) {
+//     console.log('Koalas is winner!');
+// }
+// else if (scoreDelphins === scoreKoalas && scoreDelphins >= 100 && scoreKoalas >= 100) {
+//     console.log('Both win the trophy!');
+// } else {
+//     console.log('No one win this trophy!');
+// }
+
+
+// 4-CHALENGE
+
+
+
+// const bill = 430;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
+
+
+
+
+
+
+//  2 FAYL  1-CHALENGE
+
+
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win  (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win  (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log('No team wins...');
+//   }
+// };
+
+// checkWinner(scoreDolphins, scoreKoalas);
+// checkWinner(576, 111);
+
+
+
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
+// 2-CHALENGE
+
+
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   }
+  
+
+  
+//   const bills = [125, 555, 44];
+//   const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+//   const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+  
+//   console.log(bills, tips, totals);
+  
+
+// 3-CHALENGE
+
+
+  
